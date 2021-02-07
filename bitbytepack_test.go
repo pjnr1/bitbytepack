bd2f2712715b084bdd5c85af865c446b7643ee2c
Jens Lindahl <jensctl@gmail.com>
2021-02-07 02:32:20 +0100

    add more tests

--- bitbytepack_test.go
+++ bitbytepack_test.go
@@ -9,11 +9,14 @@ func TestReadFromArray(t *testing.T) {
 	array := []byte{0x01, 0x02}
 	mask := []byte{0x0F, 0x0F}
 	want := uint(0x12)
+
 	if got := ReadFromArray(array, mask); got != want {
 		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
+
 	array = []byte{0x10, 0x02}
 	mask = []byte{0xF0, 0x0F}
+
 	if got := ReadFromArray(array, mask); got != want {
 		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
@@ -23,6 +26,7 @@ func TestReadFromArrayTypeSpecifics(t *testing.T) {
 	array := []byte{0x01, 0x02}
 	mask := []byte{0x0F, 0x0F}
 	want := uint(0x12)
+
 	if got := ReadFromArray(array, mask); got != want {
 		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
@@ -46,12 +50,39 @@ func TestWriteToArray(t *testing.T) {
 	mask := []byte{0x0F, 0x0F}
 	value := uint(0x12)
 	want := []byte{0x01, 0x02}
+
 	if got := WriteToArray(array, mask, value); !bytes.Equal(got, want) {
 		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
+
 	mask = []byte{0xF0, 0x0F}
 	want = []byte{0x10, 0x02}
+
 	if got := WriteToArray(array, mask, value); !bytes.Equal(got, want) {
 		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
+}
+
+func TestWriteToArrayTypeSpecifics(t *testing.T) {
+	array := []byte{0x00, 0x00}
+	mask := []byte{0x0F, 0x0F}
+	value := uint(0x12)
+	want := []byte{0x01, 0x02}
+
+	if got := WriteToArray8(array, mask, uint8(value)); !bytes.Equal(got, want) {
+		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
+	}
+
+	if got := WriteToArray16(array, mask, uint16(value)); !bytes.Equal(got, want) {
+		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
+	}
+
+	if got := WriteToArray32(array, mask, uint32(value)); !bytes.Equal(got, want) {
+		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
+	}
+
+	if got := WriteToArray64(array, mask, uint64(value)); !bytes.Equal(got, want) {
+		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
+	}
+
 }
