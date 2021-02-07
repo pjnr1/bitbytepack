fe05cc575a82e5b27895b4aaf40bb4b89db3b1af
Jens Lindahl <jensctl@gmail.com>
2021-02-08 00:06:24 +0100

    add Mult* functions, to allow multiple embeddings in a single []byte

--- bitbytepack.go
+++ bitbytepack.go
@@ -4,6 +4,15 @@ import (
 	"math/bits"
 )
 
+const (
+	MaxNumberOfValuesToRead = 128
+)
+
+type MaskValuePair struct {
+	mask  []byte
+	value uint
+}
+
 func ReadFromArray(array []byte, mask []byte) uint {
 	if len(array) < len(mask) {
 		return 0
@@ -36,28 +45,6 @@ func ReadFromArray(array []byte, mask []byte) uint {
 	return finalValue
 }
 
-func WriteToArray(array []byte, mask []byte, value uint) []byte {
-	if len(array) < len(mask) {
-		return []byte{}
-	}
-
-	for i := range mask {
-
-		// Reverse iteration
-		j := len(mask) - i - 1
-
-		// Shift value if mask is shifted
-		array[j] = byte(value) << bits.TrailingZeros8(mask[j])
-
-		// Apply mask
-		array[j] &= mask[j]
-
-		// Shift value by the number of bits that was written to the array
-		value >>= bits.OnesCount8(mask[j])
-	}
-	return array
-}
-
 func ReadFromArray8(v []byte, mask []byte) uint8 {
 	return uint8(ReadFromArray(v, mask))
 }
@@ -71,6 +58,31 @@ func ReadFromArray64(v []byte, mask []byte) uint64 {
 	return uint64(ReadFromArray(v, mask))
 }
 
+func WriteToArray(array []byte, mask []byte, value uint) []byte {
+	if len(array) < len(mask) {
+		return []byte{}
+	}
+
+	for i := range mask {
+
+		// Reverse iteration
+		j := len(mask) - i - 1
+
+		// Shift value if mask is shifted
+		valueByte := byte(value) << bits.TrailingZeros8(mask[j])
+
+		// Apply mask
+		valueByte &= mask[j]
+
+		// Add to array
+		array[j] |= valueByte
+
+		// Shift value by the number of bits that was written to the array
+		value >>= bits.OnesCount8(mask[j])
+	}
+	return array
+}
+
 func WriteToArray8(array []byte, mask []byte, value uint8) []byte {
 	return WriteToArray(array, mask, uint(value))
 }
@@ -82,4 +94,67 @@ func WriteToArray32(array []byte, mask []byte, value uint32) []byte {
 }
 func WriteToArray64(array []byte, mask []byte, value uint64) []byte {
 	return WriteToArray(array, mask, uint(value))
+}
+
+// Read multiple values from array using an array of masks
+func MultReadFromArray(array []byte, mask ...[]byte) []uint {
+	output := make([]uint, 0, MaxNumberOfValuesToRead)
+
+	for _, m := range mask {
+		output = append(output, ReadFromArray(array, m))
+	}
+
+	return output
+}
+
+// Read multiple values (uint8) from array using an array of masks
+func MultReadFromArray8(array []byte, mask ...[]byte) []uint8 {
+	output := make([]uint8, 0, MaxNumberOfValuesToRead)
+
+	for _, m := range mask {
+		output = append(output, ReadFromArray8(array, m))
+	}
+
+	return output
+}
+
+// Read multiple values (uint16) from array using an array of masks
+func MultReadFromArray16(array []byte, mask ...[]byte) []uint16 {
+	output := make([]uint16, 0, MaxNumberOfValuesToRead)
+
+	for _, m := range mask {
+		output = append(output, ReadFromArray16(array, m))
+	}
+
+	return output
+}
+
+// Read multiple values (uint32) from array using an array of masks
+func MultReadFromArray32(array []byte, mask ...[]byte) []uint32 {
+	output := make([]uint32, 0, MaxNumberOfValuesToRead)
+
+	for _, m := range mask {
+		output = append(output, ReadFromArray32(array, m))
+	}
+
+	return output
+}
+
+// Read multiple values (uint64) from array using an array of masks
+func MultReadFromArray64(array []byte, mask ...[]byte) []uint64 {
+	output := make([]uint64, 0, MaxNumberOfValuesToRead)
+
+	for _, m := range mask {
+		output = append(output, ReadFromArray64(array, m))
+	}
+
+	return output
+}
+
+func MultWriteToArray(array []byte, mvp ...MaskValuePair) []byte {
+	// Iterate over all Mask-Value pairs
+	for _, m := range mvp {
+		array = WriteToArray(array, m.mask, m.value)
+	}
+	return array
 }
--- bitbytepack_test.go
+++ bitbytepack_test.go
@@ -2,6 +2,7 @@ package bitbytepack
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -31,7 +32,7 @@ func TestReadFromArray(t *testing.T) {
 
 	// Test early return
 	want = 0
-	if got := ReadFromArray([]byte{}, []byte{ 0x0F }); got != want {
+	if got := ReadFromArray([]byte{}, []byte{0x0F}); got != want {
 		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
 }
@@ -69,11 +70,12 @@ func TestWriteToArray(t *testing.T) {
 		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
 
+	array = []byte{0x0a, 0x00}
 	mask = []byte{0xF0, 0x0F}
-	want = []byte{0x10, 0x02}
+	want = []byte{0x1a, 0x02}
 
 	if got := WriteToArray(array, mask, value); !bytes.Equal(got, want) {
-		t.Errorf("ReadFromArray(%x, %x) = %x, want %x", array, mask, got, want)
+		t.Errorf("WriteToArray(%x, %x) = %x, want %x", array, mask, got, want)
 	}
 
 	// Test early return
@@ -102,5 +104,117 @@ func TestWriteToArrayTypeSpecifics(t *testing.T) {
 	}
 	if got := WriteToArray64(array, mask, uint64(value)); !bytes.Equal(got, want) {
 		t.Errorf("WriteToArray64(%x, %x, %x) = %x, want %x", array, mask, uint64(value), got, want)
+	}
+}
+
+func TestMultReadFromArray(t *testing.T) {
+	array := []byte{0x12, 0x34, 0x56, 0x78}
+	masks := [][]byte{
+		{0xF0, 0xF0, 0x00, 0x00},
+		{0x0F, 0x0F, 0x00, 0x00},
+		{0xFF, 0x00, 0xFF, 0x00},
+		{0x00, 0xFF, 0x00, 0x0F}}
+	want := []uint{
+		0x13,
+		0x24,
+		0x1256,
+		0x0348}
+
+	if got := MultReadFromArray(array, masks...); !reflect.DeepEqual(want, got) {
+		t.Errorf("MultReadFromArray(%x, %x) = %x, want %x", array, masks, got, want)
+
+	}
+}
+
+func TestMultReadFromArrayTypeSpecifics(t *testing.T) {
+	array := []byte{0x12, 0x34, 0x56, 0x78}
+	masks := [][]byte{
+		{0xF0, 0xF0, 0x00, 0x00},
+		{0x0F, 0x0F, 0x00, 0x00},
+		{0xFF, 0x00, 0xFF, 0x00},
+		{0x00, 0xFF, 0x00, 0x0F},
+		{0xFF, 0xFF, 0xFF, 0xFF}}
+	want := []uint{
+		0x13,
+		0x24,
+		0x1256,
+		0x348,
+		0x12345678}
+
+	if got := MultReadFromArray(array, masks...); !reflect.DeepEqual(want, got) {
+		t.Errorf("MultReadFromArray(%x, %x) = %x, want %x", array, masks, got, want)
+	}
+
+	want8 := []uint8{
+		0x13,
+		0x24,
+		0x56,
+		0x48,
+		0x78}
+	if got := MultReadFromArray8(array, masks...); !reflect.DeepEqual(want8, got) {
+		t.Errorf("MultReadFromArray8(%x, %x) = %x, want %x", array, masks, got, want)
+	}
+
+	want16 := []uint16{
+		0x13,
+		0x24,
+		0x1256,
+		0x348,
+		0x5678}
+	if got := MultReadFromArray16(array, masks...); !reflect.DeepEqual(want16, got) {
+		t.Errorf("MultReadFromArray16(%x, %x) = %x, want %x", array, masks, got, want)
+	}
+
+	want32 := []uint32{
+		0x13,
+		0x24,
+		0x1256,
+		0x348,
+		0x12345678}
+	if got := MultReadFromArray32(array, masks...); !reflect.DeepEqual(want32, got) {
+		t.Errorf("MultReadFromArray32(%x, %x) = %x, want %x", array, masks, got, want)
+	}
+
+	want64 := []uint64{
+		0x13,
+		0x24,
+		0x1256,
+		0x348,
+		0x12345678}
+	if got := MultReadFromArray64(array, masks...); !reflect.DeepEqual(want64, got) {
+		t.Errorf("MultReadFromArray64(%x, %x) = %x, want %x", array, masks, got, want)
+	}
+}
+
+func TestMultWriteToArray(t *testing.T) {
+	array := make([]byte, 4)
+	maskValuePairs := []MaskValuePair{
+		{[]byte{0x00, 0x00, 0x00, 0xFF}, 0x78},
+		{[]byte{0x00, 0x0F, 0x0F, 0x00}, 0x46},
+		{[]byte{0xF0, 0x00, 0xF0, 0x00}, 0x15},
+		{[]byte{0x0F, 0xF0, 0x00, 0x00}, 0x23},
+	}
+	want := []byte{0x12, 0x34, 0x56, 0x78}
+	if got := MultWriteToArray(array, maskValuePairs...); !reflect.DeepEqual(got, want) {
+		t.Errorf("MultWriteToArray(%x, %x) = %x, want %x", array, maskValuePairs, got, want)
+	}
+}
+
+func BenchmarkReadFromArray(b *testing.B) {
+	array := []byte{0x81, 0x09, 0x04, 0x4A, 0x00, 0x00, 0x05, 0x01, 0xFF}
+	mask := []byte{0x00, 0x00, 0x00, 0x00, 0x0F, 0x0F, 0x0F, 0x0F, 0x00}
+
+	for i := 0; i < b.N; i++ {
+		ReadFromArray(array, mask)
+	}
+}
+
+func BenchmarkWriteToArray(b *testing.B) {
+	array := []byte{0x81, 0x09, 0x04, 0x4A, 0x00, 0x00, 0x00, 0x00, 0xFF}
+	mask := []byte{0x00, 0x00, 0x00, 0x00, 0x0F, 0x0F, 0x0F, 0x0F, 0x00}
+	value := uint(0x1234)
+
+	for i := 0; i < b.N; i++ {
+		WriteToArray(array, mask, value)
 	}
 }
